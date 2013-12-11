9a1c621b52873a4f4ca26ef90a58c1dd71923042
oferpaster <oferpaster@gmail.com>
2013-12-11 19:01:59 +0200

    -merge with gal

--- src/controler/Main_Frame_Controller.java
+++ src/controler/Main_Frame_Controller.java
@@ -1,12 +1,6 @@
 package controler;
 
-import gui.VCP_Main_Frame;
 
 public class Main_Frame_Controller {
-	public Main_Frame_Controller(VCP_Main_Frame mainFrame) {
-		super();
-		this.mainFrame = mainFrame;
-	}
 
-	VCP_Main_Frame mainFrame;
 }
